81f17e4ab4862acc9aa682dfacafa0489ed63b43
Zach Fine <zach@zachfine.com>
2025-07-17 11:54:44 -0700

    Initial Commit

---
+++ Youtube embed to URL/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Youtube embed to URL
+//
+//  Created by Zach Fine on 7/17/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ Youtube embed to URLTests/Youtube_embed_to_URLTests.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_embed_to_URLTests.swift
+//  Youtube embed to URLTests
+//
+//  Created by Zach Fine on 7/17/25.
+//
+
+import Testing
+@testable import Youtube_embed_to_URL
+
+struct Youtube_embed_to_URLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
